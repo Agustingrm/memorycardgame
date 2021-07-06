a6798b98d51d228dbcb99b00f4c6ed5f1c0330f6
Agustin Gramajo <agustingramajo@gmail.com>
2021-07-05 21:00:23 -0300

    Working on it

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+// import { useState } from "react";
+import img1 from './assets/pokemonSVG/2.svg'
+
 function App() {
-  return <div>First commit</div>;
+  // const [cards, setCards] = useState({
+  //   selectedCharacters: [],
+  // });
+
+  function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => {
+      return (
+      images[item.replace("./", "")] = r(item));
+    });
+    return images;
+  }
+
+  const images = importAll(
+    require.context("./assets/pokemonSVG", false, /\.(png|jpe?g|svg)$/)
+  );
+
+  return (
+    <div>
+      <img src={img1} alt='nada' />
+    </div>
+  );
 }
 
 export default App;
