7b582b4faf9b368480983740d0d0270b84fb6aca
Agustin Gramajo <agustingramajo@gmail.com>
2021-07-07 21:01:21 -0300

    Working on the project, now displays in a grid

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import { useState } from "react";
-// import img1 from './assets/pokemonSVG/1.svg'
-// import img2 from './assets/pokemonSVG/2.svg'
-// import img3 from './assets/pokemonSVG/3.svg'
-// import img4 from './assets/pokemonSVG/4.svg'
-// import img5 from './assets/pokemonSVG/5.svg'
-// import img6 from './assets/pokemonSVG/6.svg'
-// import img7 from './assets/pokemonSVG/7.svg'
-// import img8 from './assets/pokemonSVG/8.svg'
-// import img9 from './assets/pokemonSVG/9.svg'
 import Buttons from "./Buttons";
 
 function App() {
-  const [cards, setCards] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-
+  const [cards, setCards] = useState([0,1, 2, 3, 4, 5, 6, 7, 8]);
+  
   const handleShuffle = () => {
     let shuffledCards = cards.sort(() => Math.random() - 0.5)
     setCards([...shuffledCards])
